89e4506457ac55380cc8b61f791c9fb772ed3b73
Eric <lierichaomin@gmail.com>
2020-06-10 23:30:13 +0000

    Add charts MVP

--- portfolio/src/main/webapp/script-charts.js
+++ portfolio/src/main/webapp/script-charts.js
@@ -1,24 +1,62 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['bar', 'timeline']});
+google.charts.setOnLoadCallback(drawTimeline);
+google.charts.setOnLoadCallback(drawDonationGraph);
+google.charts.setOnLoadCallback(drawProgramGraph);
 
 /** Creates a chart and adds it to the page. */
-function drawChart() {
-  const data = new google.visualization.DataTable();
-  data.addColumn('string', 'Animal');
-  data.addColumn('number', 'Count');
+function drawTimeline() {
+
+    const date = new Date();
+    const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'Order');
+    data.addColumn('string', 'Position')
+    data.addColumn('date', 'Start');
+    data.addColumn('date', 'End');
         data.addRows([
-          ['Lions', 10],
-          ['Tigers', 5],
-          ['Bears', 15]
+            ['1', 'Intern at Boston Housing Authority', new Date(2020, 2, 20), new Date(2020, 4, 18)],
+            ['1', 'Google STEP Intern', new Date(2020, 4, 18), today],
+            ['2', 'Founder, Head of HCS for Social Good', new Date(2020, 4, 30), today],
+            ['3', 'CTO of Global Research and Consulting', new Date(2020, 4, 3), today],
+            ['4', 'Operations and Engagement Lead of Harvard Effective Altruism', new Date(2020, 4, 31), today],
         ]);
 
-  const options = {
-    'title': 'Zoo Animals',
-    'width':500,
-    'height':400
-  };
+    var options = {
+        timeline: { showRowLabels: false }
+    };
 
-  const chart = new google.visualization.PieChart(
-      document.getElementById('chart-container'));
-  chart.draw(data, options);
+    const chart = new google.visualization.Timeline(document.getElementById('timeline-container'));
+    chart.draw(data, options);
+}
+
+function drawDonationGraph() {
+    fetch('/donations-data').then(response => response.json()).then((donations) => {
+        const data = new google.visualization.DataTable();
+        data.addColumn('string', 'Cause');
+        data.addColumn('number', 'Amount donated');
+        Object.keys(donations).forEach((cause) => {
+            data.addRow([cause, donations[cause]]);
+        })
+
+        const chart = new google.charts.Bar(document.getElementById('donation-container'));
+        
+        chart.draw(data);
+    });
+}
+
+function drawProgramGraph() {
+    fetch('/charity-data').then(response => response.json()).then((charities) => {
+        const data = new google.visualization.DataTable();
+        data.addColumn('string', 'Charity');
+        data.addColumn('number', 'Votes');
+        
+        charities.forEach((charity) => {
+            data.addRow([charity.program, charity.votes]);
+        })
+
+        const chart = new google.charts.Bar(document.getElementById('charity-container'));
+
+        chart.draw(data);
+    })
 }
